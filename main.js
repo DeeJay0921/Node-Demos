087373eedb99e8ef51ae3823ae6903c155456c1b
DeeJay <1018805743@qq.com>
2018-04-13 14:09:47 +0800

    async demo

--- main.js
+++ main.js
@@ -1,17 +1,6 @@
-const log = (str) => {
-    console.log(str);
-};
+const event = require('events')
 
-const EventEmitter = require('events').EventEmitter;
-const event = new EventEmitter();
+const eventEmitter = new event.EventEmitter()
 
-event.on('testEvent',(...args) => {
-    log('hanlder1   ' + args);
-})
-event.on('testEvent', (...args) => {
-    log('handler2   ' + args)
-})
+console.log(eventEmitter)
 
-setTimeout( () => {
-    event.emit('testEvent','1','2','3');
-});
---
+++ sync.js
@@ -0,0 +1,64 @@
+// !( () => {
+//     console.log(1)
+// })() // 同步任务
+// process.nextTick( ()=> {
+//     console.log(2)
+// } ) // process.nextTick() 所有异步任务里面最快执行
+// Promise.resolve().then( ()=> {
+//     console.log(3)
+// } ) // microtask
+// process.nextTick( () => {
+//     console.log(22) // 所有的nextTick都优于Promise.then()执行
+// })
+
+// const http = require('http')
+//         url = require('url')
+
+// let server = (req,res) => {
+//     console.log(req.url)
+//     let path = url.parse(req.url).pathname
+//     console.log(path)
+//     if(path == '/') {
+//         res.writeHead(200,{
+//             'Content-Type': 'text/plain'
+//         })
+//         res.write('hello node')
+//     }else if( path == '/about') {
+//         res.writeHead(200,{
+//             'Content-Type': 'text/plain'
+//         })
+//         res.write('ab page')
+//     }else if( path == '/error') {
+//         res.writeHead(200,{
+//             'Content-Type': 'text/plain'
+//         })
+//         res.write('error')
+//     }
+//     res.end();
+// }
+// http.createServer(server).listen(3000, () => {
+//     console.log('done!')
+// })
+
+function getTime () {
+    return new Promise((resolve,reject) => {
+        setTimeout( ()=> {
+            const date = Date.now()
+            if(date % 2 == 0) {
+                resolve(date)
+            }else {
+                reject(date)
+            }
+        },500)
+    })
+}
+
+async function printDate () {
+    const res = await getTime().then( (date) => {
+        console.log(`为偶数: ${date}`)
+    }).catch( (err) => {
+        console.log('为奇数')
+    })
+    // console.log(`为偶数: ${res}`)
+}
+printDate()
